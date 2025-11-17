d80ea60e295440feb69925f34a4caf3615ba6f76
William 'William <wi1123.syd@gmail.com>
2025-11-17 18:20:16 +0700

    go-api-service: feat: finished GetSymbols repo layer

--- internal/api/repo/go-api-service.go
+++ internal/api/repo/go-api-service.go
@@ -1,12 +1,16 @@
 package repo
 
 import (
+	"financial-data-backend-2/internal/models"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type RepoItf interface {
-	GetSymbols(*gin.Context) error
+	GetSymbols(*gin.Context) ([]models.SymbolDocument, error)
 }
 
 type Repo struct {
@@ -18,6 +22,23 @@ func NewRepo(symbolCollection, tradeCollection *mongo.Collection) *Repo {
 	return &Repo{sc: symbolCollection, tc: tradeCollection}
 }
 
-func (rp *Repo) GetSymbols(ctx *gin.Context) error {
-	return nil
+func (rp *Repo) GetSymbols(ctx *gin.Context) ([]models.SymbolDocument, error) {
+	c := ctx.Request.Context()
+	results, err := rp.sc.Find(c, bson.D{}, options.Find().SetSort(
+		bson.D{{Key: "symbol", Value: 1}}))
+	if err != nil {
+		return nil, err
+	}
+
+	symbols := make([]models.SymbolDocument, 0)
+	defer results.Close(c)
+	for results.Next(c) {
+		var symbol models.SymbolDocument
+		if err = results.Decode(&symbol); err != nil {
+			return nil, err
+		}
+		symbols = append(symbols, symbol)
+	}
+
+	return symbols, nil
 }
