057a78df5f1e6f8c90ee85c6fab1861c461314c5
William 'William <wi1123.syd@gmail.com>
2025-11-17 10:05:46 +0700

    feat: go-api-service: load config and connect to MongoDB

--- cmd/go-api-service/main.go
+++ cmd/go-api-service/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "log"
+import (
+	"context"
+	"financial-data-backend-2/internal/config"
+	mongoGo "financial-data-backend-2/internal/mongo"
+	"log"
+	"time"
+)
 
 func main() {
-	log.Printf("Hello World")
+	// - Load Configuration
+	cfg, err := config.LoadConfig("config/config.yml")
+	if err != nil {
+		log.Fatalf("Error loading configuration: %v", err)
+	}
+
+	// - Setup MongoDB database
+	DB, err := mongoGo.ConnectDB(cfg.MongoDB.URL)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	defer func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := DB.Disconnect(ctx); err != nil {
+			log.Fatalf("Error during MongoDB disconnect: %v", err)
+		}
+		log.Println("MongoDB client disconnected.")
+	}()
 }
