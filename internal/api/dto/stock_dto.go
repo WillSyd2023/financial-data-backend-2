a0f0f9e559abd17a76d41e34226fef92c66d8d16
William 'William <wi1123.syd@gmail.com>
2025-11-17 14:28:42 +0700

    go-service-api: feat: initial GetSymbols DTOs

--- internal/api/dto/stock_dto.go
+++ internal/api/dto/stock_dto.go
@@ -1,1 +1,15 @@
 package dto
+
+import "time"
+
+// GetSymbols
+
+type GetSymbolsSingle struct {
+	Symbol      string    `json:"symbol"`
+	TradeCount  int64     `json:"trade_count"`
+	LastTradeAt time.Time `json:"last_trade_at"`
+}
+
+type GetSymbolsRes struct {
+	Available []GetSymbolsSingle `json:"available"`
+}
