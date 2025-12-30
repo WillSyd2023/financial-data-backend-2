1f5d47edc61978223343ec9fa18e0cb22c3a2713
William <wi1123.syd@gmail.com>
2025-12-30 15:26:39 +0700

    feat: python-analytics: set up how to load config

--- python-analytics/engine.py
+++ python-analytics/engine.py
@@ -1,11 +1,33 @@
+'''Real-time Python financial analytics engine'''
 import asyncio
+import sys
+import logging
+import yaml
+from aiokafka import AIOKafkaConsumer
 
 class AnalyticsEngine:
     def __init__(self):
         pass
 
     async def run(self):
-        print('Hello World!')
+        '''
+        # Setup Kafka Consumer
+        consumer = AIOKafkaConsumer(
+            KAFKA_TOPIC,
+            bootstrap_servers=KAFKA_BROKER,
+            group_id="analytics-engine-group"
+        )
+        '''
+        logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+        try:
+            with open("../config/config.yml", "r", encoding="utf-8") as f:
+                config = yaml.safe_load(f)
+        except yaml.YAMLError as exc:
+            logging.critical("Failed to load config: %s", exc)
+            sys.exit(1)
+
+        print(config)
 
 if __name__ == '__main__':
     engine = AnalyticsEngine()
