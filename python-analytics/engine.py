31fe81dc5fec6bf35005bf5d91410e968e3af8ac
William <wi1123.syd@gmail.com>
2025-12-30 16:38:50 +0700

    python-analytics-engine: feat: set up Docker

--- python-analytics/engine.py
+++ python-analytics/engine.py
@@ -5,29 +5,27 @@ import logging
 import yaml
 from aiokafka import AIOKafkaConsumer
 
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
 class AnalyticsEngine:
     def __init__(self):
         pass
 
     async def run(self):
-        '''
-        # Setup Kafka Consumer
-        consumer = AIOKafkaConsumer(
-            KAFKA_TOPIC,
-            bootstrap_servers=KAFKA_BROKER,
-            group_id="analytics-engine-group"
-        )
-        '''
-        logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-
+        # Load config, but just if fail
         try:
-            with open("../config/config.yml", "r", encoding="utf-8") as f:
+            with open('../config/config.yml', 'r', encoding='utf-8') as f:
                 config = yaml.safe_load(f)
         except yaml.YAMLError as exc:
-            logging.critical("Failed to load config: %s", exc)
+            logging.critical('Failed to load config: %s', exc)
             sys.exit(1)
 
-        print(config)
+        # Setup Kafka Consumer
+        consumer = AIOKafkaConsumer(
+            config['kafka']['topic'],
+            bootstrap_servers=config['kafka']['broker_url'],
+            group_id='analytics-engine-group'
+        )
 
 if __name__ == '__main__':
     engine = AnalyticsEngine()
